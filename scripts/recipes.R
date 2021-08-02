63b7c9934c08fedbcfffdd44ddf5988d47f3533c
Hendrik <67455442+Hendrikjilderda@users.noreply.github.com>
2021-08-02 12:03:00 +0200

    created recipe

---
+++ scripts/recipes.R
@@ -0,0 +1,10 @@
+GCR_recipe <- recipe(Risk ~ ., data = GCR_train) %>%
+  update_role(X, new_role = "ID") %>%
+  step_other(Purpose, threshold = 0.05) # is dit wel nodig? van 8 naar 5 
+  
+# step functions toevoegen?
+
+
+GCR_prep <- prep(GCR_recipe)
+
+GCR_juice <-juice(GCR_prep)
