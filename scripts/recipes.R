088a1a366a1d4311feafbe5b2fb506595a34449d
Hendrik <67455442+Hendrikjilderda@users.noreply.github.com>
2021-08-03 12:46:17 +0200

    Update recipes.R

--- scripts/recipes.R
+++ scripts/recipes.R
@@ -1,17 +1,18 @@
 GCR_recipe <- recipe(Risk ~ ., data = GCR_train) %>%
-  step_impute_knn(Saving.accounts,  Checking.account, k = 5) %>%
+  step_impute_knn(Saving.accounts,  Checking.account, neighbors = 5) %>%
     
   step_rm(X) %>%
   step_other(Purpose, threshold = 0.10) %>% # is dit wel nodig? van 8 naar 4
 
 
-step_mutate(
-  Credit.amount = ifelse(Credit.amount > 4000, 'high',
-                         ifelse(Credit.amount > 2000, 'medium', 'low')),
-
-  Duration = ifelse(Duration > 25, 'long',
-                    ifelse(Duration > 15, 'medium','short'))
-) %>%
+# step_mutate(
+#   Credit.amount = ifelse(Credit.amount > 4000, 'high',
+#                          ifelse(Credit.amount > 2000, 'medium', 'low')),
+# 
+#   Duration = ifelse(Duration > 25, 'long',
+#                     ifelse(Duration > 15, 'medium','short'))
+# ) %>%
+  
   step_string2factor(all_nominal()) %>%
 
   step_dummy(all_nominal(), -Risk) 
