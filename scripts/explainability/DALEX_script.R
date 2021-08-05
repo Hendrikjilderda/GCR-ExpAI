5f4dedeb11f3b8ac56d9c3787ae29051bba3b9ad
Hendrik <67455442+Hendrikjilderda@users.noreply.github.com>
2021-08-05 09:01:36 +0200

    extra check voor variabelen

--- scripts/explainability/DALEX_script.R
+++ scripts/explainability/DALEX_script.R
@@ -20,87 +20,87 @@
 function_amount <- 4
 
 
-explainer <- tm_explainer(workflow, dataset, target_variable, label)
-
-list <- vector(mode = "list", length = function_amount)
-plot_list <- vector(mode = "list", length = 0)
-
-plot_counter <- 0 
-
-######
-
-if(!is.null(case)){
-  plot_SHAP <- SHAP(case, explainer)
-  list <- c(list, 1)
-  
+if(!exists('workflow') || !exists('dataset') || !exists('target_variable')){
+  stop('not able to make explainer because of missing variables')
 } else {
-  warning('case not specified')
-}
 
-
-######
-
-if (!is.null(variables) && !is.null(case)){
-  plot_CP <- CP(case, variables, explainer)
+  explainer <- tm_explainer(workflow, dataset, target_variable, label)
+  
+  list <- vector(mode = "list", length = function_amount)
+  plot_list <- vector(mode = "list", length = 0)
+  
+  plot_counter <- 0 
+  
+  ######
+  
+  if(!is.null(case)){
+    plot_SHAP <- SHAP(case, explainer)
+    list <- c(list, 1)
+    
+  } else {
+    warning('case not specified')
+  }
   
   
-  list <- c(list, 2)
-} else{
-  warning('variables or/and case not specified')
-
-}
-
-
-######
-plot_VIP <- VIP(explainer)
-list <- c(list, 3)
-
-######
-plot_PDP <- PDP(var, explainer)
-list <- c(list, 4)
-
-
-
-
+  ######
+  
+  if (!is.null(variables) && !is.null(case)){
+    plot_CP <- CP(case, variables, explainer)
+    
+    
+    list <- c(list, 2)
+  } else{
+    warning('variables or/and case not specified')
+  
+  }
+  
+  
+  ######
+  plot_VIP <- VIP(explainer)
+  list <- c(list, 3)
+  
+  ######
+  plot_PDP <- PDP(var, explainer)
+  list <- c(list, 4)
 
 #combining plots into one plot
 ################################################################################
-for(plot in list){
-  switch (plot,
-    1 = {
-      if(!exists('plot_list')) {
-        plot_list <- vector(mode = "list", length = 1)
-        plot_list[1] <- plot_SHAP
-      } else{
-        plot_list <- c(plot_list, plot_SHAP)
+  for(plot in list){
+    switch (plot,
+      1 = {
+        if(!exists('plot_list')) {
+          plot_list <- vector(mode = "list", length = 1)
+          plot_list[1] <- plot_SHAP
+        } else{
+          plot_list <- c(plot_list, plot_SHAP)
+        }
+      },
+      
+      2 = {
+        if(!exists('plot_list')) {
+          plot_list <- vector(mode = "list", length = 1)
+          plot_list[1] <- plot_CP
+        } else{
+          plot_list <- c(plot_list, plot_CP)
+        }
+      },
+      
+      3 = {
+        if(!exists('plot_list')) {
+          plot_list <- vector(mode = "list", length = 1)
+          plot_list[1] <- plot_VIP
+        } else{
+          plot_list <- c(plot_list, plot_VIP)
+        }
+      },
+      
+      4 = {
+        plot_list <- c(plot_list, plot_PDP)
       }
-    },
-    
-    2 = {
-      if(!exists('plot_list')) {
-        plot_list <- vector(mode = "list", length = 1)
-        plot_list[1] <- plot_CP
-      } else{
-        plot_list <- c(plot_list, plot_CP)
-      }
-    },
-    
-    3 = {
-      if(!exists('plot_list')) {
-        plot_list <- vector(mode = "list", length = 1)
-        plot_list[1] <- plot_VIP
-      } else{
-        plot_list <- c(plot_list, plot_VIP)
-      }
-    },
-    
-    4 = {
-      plot_list <- c(plot_list, plot_PDP)
-    }
-  )
+    )
+  }
+  
+  #zou in theorie moeten werken?!
+  gridExtra::grid.arrange(plot_list)
+
 }
-
-#zou in theorie moeten werken?!
-gridExtra::grid.arrange(plot_list)
-
-
